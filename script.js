d4f71e7bf67bbd94c217b2fdd114e58cfb158bb7
unknown <karpagalakshmi314@gmail.com>
2024-08-20 14:48:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+alert('test');
+     
+setTimeout(() => document.getElementsByClassName('name')[0].innerHTML='change name',3000)
+setTimeout()) => document.getElementByClassName('name')[1].innerHTML ='',8000)
+alert(6+6)
+console.log('output')
+var para = document.createElement('p')
+for(i = 0 ; i < 10 ; i++)
+{
+    console.log
+}
