111389ee0f0ff0555dcc9209cef72a14f4f3a201
unknown <karpagalakshmi314@gmail.com>
2024-08-20 15:04:21 +0530

    paragrapchange

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 alert('test');
      
 setTimeout(() => document.getElementsByClassName('name')[0].innerHTML='change name',3000)
-setTimeout()) => document.getElementByClassName('name')[1].innerHTML ='',8000)
+setTimeout(()=> document.getElementByClassName('name')[1].innerHTML ='',8000)
 alert(6+6)
 console.log('output')
 var para = document.createElement('p')
